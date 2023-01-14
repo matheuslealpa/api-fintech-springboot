a782bba418da261005451422a3b6817a3c988ad6
Matheus Leal <matheuslealpa@gmail.com>
2023-01-14 15:42:13 -0300

    feat: client domain crud

--- src/main/java/com/example/app/controller/ClienteController.java
+++ src/main/java/com/example/app/controller/ClienteController.java
@@ -5,11 +5,9 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.app.domain.Cliente;
 import com.example.app.service.ClienteService;
@@ -25,13 +23,31 @@ public class ClienteController {
     private final ClienteService clienteService;
 
     @GetMapping
+    @ResponseStatus(HttpStatus.OK)
     public Page<Cliente> findAll(RSQLParam q, Pageable pageable){
         return clienteService.findAll(q, pageable);
     }
 
-    @GetMapping("/id")
+    @GetMapping("/{id}")
     public ResponseEntity<Optional<Cliente>> findById(@PathVariable Long id){
         return ResponseEntity.ok(clienteService.findById(id));
     }
-    
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Cliente> insert(@RequestBody Cliente cliente){
+        return ResponseEntity.ok(clienteService.insert(cliente));
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public ResponseEntity<Cliente> update(@RequestBody Cliente cliente, @PathVariable Long id){
+        return ResponseEntity.ok(clienteService.update(cliente, id));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id){
+        clienteService.delete(id);
+    }
 }
